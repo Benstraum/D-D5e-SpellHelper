9160f09506ce516aa45a18d69b8c06a732ea002c
ben straumann <benstraum@gmail.com>
2020-08-19 17:14:40 -0500

    starter repo for simple api based mobile app using redux

--- index.js
+++ index.js
@@ -1,8 +1,36 @@
 import { registerRootComponent } from 'expo';
+import React from 'react';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import createSagaMiddleware from 'redux-saga';
+
+import rootReducer from './redux/reducers'; // imports ./redux/reducers/index.js
+import rootSaga from './redux/sagas'; // imports ./redux/sagas/index.js
 
 import App from './App';
+
+const sagaMiddleware = createSagaMiddleware();
+
+const middlewareList = process.env.NODE_ENV === 'development' ?
+  [sagaMiddleware] :
+  [sagaMiddleware];
+
+const store = createStore(
+  rootReducer,
+  applyMiddleware(...middlewareList),
+);
+
+sagaMiddleware.run(rootSaga);
+//set up react native redux with provider around the app
+const RNRedux = () => (
+  <Provider store = { store }>
+    <App />
+  </Provider>
+)
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in the Expo client or in a native build,
 // the environment is set up appropriately
-registerRootComponent(App);
+
+//calling RNredux allows app to be called with the provider store surrounding it
+registerRootComponent(RNRedux);
---
+++ src/Feed.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { StyleSheet, Text, View, ImageBackground, Button } from 'react-native';
+
+import { connect } from 'react-redux';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createDrawerNavigator } from '@react-navigation/drawer'
+
+
+import { navigationRef } from './RootNavigation';
+
+
+const Stack = createStackNavigator();
+const Drawer = createDrawerNavigator();
+
+function Feed () {
+        return (
+            <NavigationContainer ref={navigationRef}>
+                <Drawer.Navigator initialRouteName={'Home'}>
+                    <Drawer.Screen name="Home"  />
+                </Drawer.Navigator>
+            </NavigationContainer>
+        )
+}
+export default connect()(Feed)
